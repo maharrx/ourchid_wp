61aa6d6e738e5e206cc43c51a2530a4a6c7bc667
Raju Maharjan <36067002+maharrx@users.noreply.github.com>
2024-04-29 11:15:43 -0500

    Update single-research.php

--- wp-content/themes/ourchid/single-research.php
+++ wp-content/themes/ourchid/single-research.php
@@ -53,11 +53,9 @@ get_header(); ?>
 									<h2 class="m0 p0 pb3">Investigators</h2>
 								</div>
 									<!-- // Load posts loop. -->
-
-								<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>	
+								<div class="clearfix mxn3 pb3">
+									<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>	
 									
-									<div class="clearfix mxn3 pb3">
-										
 										<div class="center col col-sm-4 col-md-12 px3">
 											
 											<figure class="circle mx-auto">                           
@@ -73,11 +71,9 @@ get_header(); ?>
 											<?php the_content(); ?>
 
 										</div>
-										
 
-									</div>		
-
-								<?php endwhile; ?>				
+									<?php endwhile; ?>				
+								</div>		
 
 							<?php  else: ?>
 								<?php echo "No content found!"; ?>								
