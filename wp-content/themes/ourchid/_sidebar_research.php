0d48d11fc3669cfb46ad0f743887fb4f74d7a4e1
Raju Maharjan <36067002+maharrx@users.noreply.github.com>
2024-05-01 12:43:26 -0500

    Update _sidebar_research.php
    funding sidebar fixes

--- wp-content/themes/ourchid/_sidebar_research.php
+++ wp-content/themes/ourchid/_sidebar_research.php
@@ -1,45 +1,40 @@
 
 
     <!-- funding start -->
-    <div class="funding clearfix mxn2 pb4">
-        <h2 class="m0 px2 pb3">Funding</h2>
-
             
-        <?php 
-            $entries = get_post_meta( get_the_ID(), 'funding_repeat_group', true );
+    <?php $entries = get_post_meta( get_the_ID(), 'funding_repeat_group', true );?>    
 
-            foreach ( (array) $entries as $key => $entry ) {
-                $img = $title = $desc = $caption = '';
-
-                if ( isset( $entry['title'] ) ) {
-                    $title = esc_html( $entry['title'] );
-                }
-
-                if ( isset( $entry['url'] ) ) {
-                    $url = esc_html( $entry['url'] );
-                }
-
-                if ( isset( $entry['image_id'] ) ) {
-                    $img = wp_get_attachment_image( $entry['image_id'], 'share-pick', null, array(
-                        'class' => 'thumb block',
-                    ) );
-                    
-                }
-
-                $caption = isset( $entry['image_caption'] ) ? wpautop( $entry['image_caption'] ) : '';
-        ?>      
-                                
-        <div class="center sm-col sm-col-6 md-col-4 lg-col-12 px2 mb3">
-            <a class="block border p2 bg-default border-light" href="<?php echo $url; ?>" target="_blank">
-                <?php echo ($img); ?>
-            </a>
-        </div>
-
-        <?php   
+    <?php if( !empty( $entries ) ) : ?>
+        <?php foreach ( (array) $entries as $key => $entry ) {
+            $img = $title = $desc = $caption = '';
+            if ( isset( $entry['title'] ) ) {
+                $title = esc_html( $entry['title'] );
             }
-        ?>
 
-    </div>
+            if ( isset( $entry['url'] ) ) {
+                $url = esc_html( $entry['url'] );
+            }
+
+            if ( isset( $entry['image_id'] ) ) {
+                $img = wp_get_attachment_image( $entry['image_id'], 'share-pick', null, array(
+                    'class' => 'thumb block',
+                ) );
+                        
+            }
+
+            $caption = isset( $entry['image_caption'] ) ? wpautop( $entry['image_caption'] ) : '';
+        ?>  
+        <div class="funding clearfix mxn2 pb4">
+            <div class="sm-col sm-col-6 md-col-4 lg-col-12 px2 mb3">
+                <h2 class="m0 pb3">Funding</h2>
+                <a class="block border p2 bg-default border-light" href="<?php echo $url; ?>" target="_blank">
+                    <?php echo ($img); ?>
+                </a>
+            </div>
+        </div>    
+        <?php } ?> <!-- end for each -->
+    <?php endif; ?> <!-- end if-->
+
     <!-- funding end -->
 
 
