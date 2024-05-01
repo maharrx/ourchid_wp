b313f08c83f413aef6b1f899b8e66957dded234b
Raju Maharjan <36067002+maharrx@users.noreply.github.com>
2024-05-01 15:21:06 -0500

    Update _sidebar_research.php

--- wp-content/themes/ourchid/_sidebar_research.php
+++ wp-content/themes/ourchid/_sidebar_research.php
@@ -5,34 +5,38 @@
     <?php $entries = get_post_meta( get_the_ID(), 'funding_repeat_group', true );?>    
 
     <?php if( !empty( $entries ) ) : ?>
-        <?php foreach ( (array) $entries as $key => $entry ) {
-            $img = $title = $desc = $caption = '';
-            if ( isset( $entry['title'] ) ) {
-                $title = esc_html( $entry['title'] );
-            }
-
-            if ( isset( $entry['url'] ) ) {
-                $url = esc_html( $entry['url'] );
-            }
-
-            if ( isset( $entry['image_id'] ) ) {
-                $img = wp_get_attachment_image( $entry['image_id'], 'share-pick', null, array(
-                    'class' => 'thumb block',
-                ) );
-                        
-            }
-
-            $caption = isset( $entry['image_caption'] ) ? wpautop( $entry['image_caption'] ) : '';
-        ?>  
         <div class="funding clearfix mxn2 pb4">
-            <div class="sm-col sm-col-6 md-col-4 lg-col-12 px2 mb3">
+            <div class="sm-col sm-col-6 md-col-4 lg-col-12 px2">
                 <h2 class="m0 pb3">Funding</h2>
-                <a class="block border p2 bg-default border-light" href="<?php echo $url; ?>" target="_blank">
+        
+                <?php foreach ( (array) $entries as $key => $entry ) {
+                    $img = $title = $desc = $caption = '';
+                    if ( isset( $entry['title'] ) ) {
+                        $title = esc_html( $entry['title'] );
+                    }
+
+                    if ( isset( $entry['url'] ) ) {
+                        $url = esc_html( $entry['url'] );
+                    }
+
+                    if ( isset( $entry['image_id'] ) ) {
+                        $img = wp_get_attachment_image( $entry['image_id'], 'share-pick', null, array(
+                            'class' => 'thumb block',
+                        ) );
+                                
+                    }
+
+                    $caption = isset( $entry['image_caption'] ) ? wpautop( $entry['image_caption'] ) : '';
+                ?>  
+
+                <a class="block border p2 bg-default border-light mb2" href="<?php echo $url; ?>" target="_blank">
                     <?php echo ($img); ?>
                 </a>
+
+                <?php } ?> <!-- end for each -->
+
             </div>
-        </div>    
-        <?php } ?> <!-- end for each -->
+        </div> 
     <?php endif; ?> <!-- end if-->
 
     <!-- funding end -->
@@ -52,7 +56,7 @@
                                                 
             <?php while ( $pi_query->have_posts() ) : $pi_query->the_post(); ?> 
 
-                <div class="center sm-col sm-col-6 md-col-4 lg-col-12 px2 mb3">
+                <div class="center sm-col sm-col-6 md-col-4 lg-col-12 px2 mb2">
 
                     <div class="profile bg-default border border-light py3 px2 center relative">
 
@@ -97,7 +101,7 @@
                                 
             <?php while ( $co_pis_query->have_posts() ) : $co_pis_query->the_post(); ?> 
 
-                <div class="center sm-col sm-col-6 md-col-4 lg-col-12 px2 mb3">
+                <div class="center sm-col sm-col-6 md-col-4 lg-col-12 px2 mb2">
 
                     <div class="profile bg-default border border-light py3 px2 center">
 
