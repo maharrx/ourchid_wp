74f39c64bad91f6a2635e4929c9c341d451652e8
Raju Maharjan <36067002+maharrx@users.noreply.github.com>
2025-04-25 14:23:56 -0500

    resources cpt access to admin

--- wp-content/themes/ourchid/cpt_resource.php
+++ wp-content/themes/ourchid/cpt_resource.php
@@ -65,6 +65,24 @@ function resources_post_type() {
         $role->add_cap('edit_published_resources');
         $role->add_cap('delete_published_resources');
     }
+
+    // Ensure admins can manage all resources
+    $admin_role = get_role('administrator');
+    if ($admin_role) {
+        $admin_role->add_cap('edit_resource');
+        $admin_role->add_cap('read_resource');
+        $admin_role->add_cap('delete_resource');
+        $admin_role->add_cap('edit_resources');
+        $admin_role->add_cap('edit_others_resources');
+        $admin_role->add_cap('publish_resources');
+        $admin_role->add_cap('read_private_resources');
+        $admin_role->add_cap('delete_resources');
+        $admin_role->add_cap('delete_private_resources');
+        $admin_role->add_cap('delete_published_resources');
+        $admin_role->add_cap('delete_others_resources');
+        $admin_role->add_cap('edit_private_resources');
+        $admin_role->add_cap('edit_published_resources');
+    }
  }
  add_action( 'init', 'resources_post_type');
  
