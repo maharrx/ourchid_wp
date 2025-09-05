d965bfd2285b9a9a3939891b192a01b844ce70a4
Raju Maharjan <36067002+maharrx@users.noreply.github.com>
2025-09-04 21:59:48 -0500

    Update cpt_members.php

--- wp-content/themes/ourchid/cpt_members.php
+++ wp-content/themes/ourchid/cpt_members.php
@@ -33,7 +33,7 @@ function members_post_type() {
         'public' => true,
         'has_archive' => true,
         'menu_icon'   => 'dashicons-media-text',
-        'rewrite' => array('slug' => 'people'),
+        'rewrite' => array('slug' => 'members'),
         'supports' => array('gutenberg','title', 'editor', 'thumbnail','excerpt'),
         'show_ui' => true,
         'show_in_menu' => true,
